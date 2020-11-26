8397e8cf6e14b6c538a6e5d109dc41894e0f25a0
BuildTools <unconfigured@null.spigotmc.org>
2020-11-25 19:56:26 -0700

    Add harvest tool and level.

--- src/main/java/rrajagopal/suntotem/common/init/SunTotemBlocks.java
+++ src/main/java/rrajagopal/suntotem/common/init/SunTotemBlocks.java
@@ -3,6 +3,7 @@ package rrajagopal.suntotem.common.init;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,6 +14,10 @@ public class SunTotemBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SunTotem.MODID);
 
     public static final RegistryObject<Block> BLOCK_SUN_TOTEM = BLOCKS.register("sun_totem", () -> new BlockSunTotem(
-            AbstractBlock.Properties.create(Material.GLASS).hardnessAndResistance(50, 100))
+            AbstractBlock.Properties.create(Material.GLASS)
+                    .hardnessAndResistance(50, 100)
+                    .harvestTool(ToolType.PICKAXE)
+                    .harvestLevel(4)
+            )
     );
 }
